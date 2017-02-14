74442260e56fd7e7968eb202a76428ac2db97fa4
flinga <flinga@cs.washington.edu>
2017-02-13 19:51:24 -0800

    " -> '

--- barchart.js
+++ barchart.js
@@ -20,15 +20,15 @@ function barchart(id, height, data, negColor, posColor) {
   const SVG_MARGINS = { top: 10, bottom: 10, left: 10, right: 10 };
   const AXIS_OFFSET = 5;
   const DATA_MAX = Math.max(...data.map(function(d) { return Math.abs(d.value); }));
-  const BAR_COLORS = interpolateColors(negColor || "#dc3912", "lightgrey", posColor || "#109618", 256);
+  const BAR_COLORS = interpolateColors(negColor || '#dc3912', 'lightgrey', posColor || '#109618', 256);
   var BY_NAME = true;
   var DESCENDING = true;
 
   // clear out old DOM elements
   flushContents(id);
 
-  var container = new SVGContainer(id, "barchart", "barchartSVG", resize, SVG_MARGINS, height);
-  addDropShadowFilter(container.SVG, "shadow");
+  var container = new SVGContainer(id, 'barchart', 'barchartSVG', resize, SVG_MARGINS, height);
+  addDropShadowFilter(container.SVG, 'shadow');
 
   container.resize();
 
@@ -66,15 +66,15 @@ function barchart(id, height, data, negColor, posColor) {
   var xAxis = d3.axisTop(scaleX);
 
   // add the axes to the svg (add these before the bars so the bars will be on top)
-  var xLabels = svg.append("g")
-                  .attr("id", "xticks")
-                  .attr("class", "axis")
-                  .attr("transform", "translate(" + marginXLabel + "," + marginYLabel + ")")
+  var xLabels = svg.append('g')
+                  .attr('id', 'xticks')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(' + marginXLabel + ',' + marginYLabel + ')')
                   .call(xAxis.tickSize(-marginYChart - AXIS_OFFSET, 0, 0));
 
   // bars
-  var bars = new Cells(svg, "bars", data, key,
-    // -1 for pos bars -> no overlap on "0" center tick
+  var bars = new Cells(svg, 'bars', data, key,
+    // -1 for pos bars -> no overlap on '0' center tick
     function(d) { return scaleX(0) - (d.value < 0 ? scaleWidth(Math.abs(d.value)) : -1); },
     // add half of (step - bandwidth) to account for paddingInner/Outer
     function(d) { return scaleY(d.key) + (scaleHeight.step() - scaleHeight.bandwidth()) / 2; },
@@ -83,36 +83,36 @@ function barchart(id, height, data, negColor, posColor) {
     function(d) { return scaleFill(d.value); });
 
   // labels at left
-  var barLabels = new Labels(svg, "labels", "axis", labels, function() { return marginYChart; },
-                            scaleHeight.step, false, 10, "left");
+  var barLabels = new Labels(svg, 'labels', 'axis', labels, function() { return marginYChart; },
+                            scaleHeight.step, false, 10, 'left');
 
   // tooltip for bars
-  var tip = d3.tip().attr("class", "d3-tip")
+  var tip = d3.tip().attr('class', 'd3-tip')
                     .direction(function(d) { return d.value < 0 ? 'e' : 'w'; })
                     .offset(function(d) { return d.value < 0 ? [0, 10] : [0, -10]; })
                     .html(function(d) {
-                      return "<table>" +
-                              "<tr><td>Variable</td><td>" + d.key + "</td></tr>" +
-                              "<tr><td>Coefficient</td><td>" + round(d.value, 7) + "</td></tr>" +
-                             "</table>";
+                      return '<table>' +
+                              '<tr><td>Variable</td><td>' + d.key + '</td></tr>' +
+                              '<tr><td>Coefficient</td><td>' + round(d.value, 7) + '</td></tr>' +
+                             '</table>';
                     });
 
   // invoke tooltip
   svg.call(tip);
 
   // add ids to labels so they can be bolded on hover
-  barLabels.group.selectAll("text").attr("id", function() { return htmlEscape(this.innerHTML); });
+  barLabels.group.selectAll('text').attr('id', function() { return htmlEscape(this.innerHTML); });
 
   // bind event listeners
-  bars.addListener("mouseover", function(d) {
-    barLabels.group.select("#" + htmlEscape(d.key)).classed("bold", true);
+  bars.addListener('mouseover', function(d) {
+    barLabels.group.select('#' + htmlEscape(d.key)).classed('bold', true);
     tip.show(d);
   });
-  bars.addListener("mouseout", function(d) {
-    barLabels.group.select("#" + htmlEscape(d.key)).classed("bold", false);
+  bars.addListener('mouseout', function(d) {
+    barLabels.group.select('#' + htmlEscape(d.key)).classed('bold', false);
     tip.hide();
   });
-  bars.addListener("click", sortBars);
+  bars.addListener('click', sortBars);
 
   // last setup before initial bar transition
   marginsSetup(w, h);
@@ -121,24 +121,24 @@ function barchart(id, height, data, negColor, posColor) {
   positionAllElements();
 
   // vertical line next to textual lables at left
-  svg.append("g")
-    .attr("id", "labels")
-    .attr("class", "axis")
-    .append("path")
-    .attr("d", "M " + barLabels.anchor[0] + " " + barLabels.anchor[1] + " L " + barLabels.anchor[0] + " " + h);
+  svg.append('g')
+    .attr('id', 'labels')
+    .attr('class', 'axis')
+    .append('path')
+    .attr('d', 'M ' + barLabels.anchor[0] + ' ' + barLabels.anchor[1] + ' L ' + barLabels.anchor[0] + ' ' + h);
 
   // custom initialization + transition
-  bars.selection.attr("x", scaleX(0))
-                .attr("y", bars.attrs.y)
-                .attr("height", bars.attrs.height)
-                .attr("width", 0)
-                .attr("fill", "white");
+  bars.selection.attr('x', scaleX(0))
+                .attr('y', bars.attrs.y)
+                .attr('height', bars.attrs.height)
+                .attr('width', 0)
+                .attr('fill', 'white');
   bars.selection.transition()
                 .duration(1000)
                 .delay(function(d, i) { return i * 25; })
-                .attr("x", bars.attrs.x)
-                .attr("width", bars.attrs.width)
-                .attr("fill", bars.attrs.fill);
+                .attr('x', bars.attrs.x)
+                .attr('width', bars.attrs.width)
+                .attr('fill', bars.attrs.fill);
 
   function marginsSetup(w, h) {
     marginXLabel = Math.ceil(barLabels.getBox().width);
@@ -162,12 +162,12 @@ function barchart(id, height, data, negColor, posColor) {
   function positionAllElements() {
     bars.position();
     barLabels.position();
-    xLabels.attr("transform", "translate(" + (marginXLabel + AXIS_OFFSET) + "," + marginYLabel + ")")
+    xLabels.attr('transform', 'translate(' + (marginXLabel + AXIS_OFFSET) + ',' + marginYLabel + ')')
           .call(xAxis.tickSize(-marginYChart - AXIS_OFFSET, 0, 0));
   }
 
   function updateVisAllElements() {
-    bars.updateVis(["x", "y", "width", "height", "fill"]);
+    bars.updateVis(['x', 'y', 'width', 'height', 'fill']);
     xLabels.call(xAxis.tickSize(-marginYChart - AXIS_OFFSET, 0, 0));
     barLabels.updateVisNT();
   }
@@ -206,7 +206,7 @@ function barchart(id, height, data, negColor, posColor) {
                   .duration(1000)
                   // TODO find a way to sync bars with labels
                   //.delay(function(d) { return 500 * Math.abs(d.value) / DATA_MAX; })
-                  .attr("y", bars.attrs.y);
+                  .attr('y', bars.attrs.y);
     barLabels.updateNames(labels);
     barLabels.updateVis(1000);
   }
