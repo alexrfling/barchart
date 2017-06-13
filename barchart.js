7963c01989e97f658557adda12e54196e1571e40
flinga <flinga@cs.washington.edu>
2017-06-13 10:15:44 -0700

    clean up and comments

--- barchart.js
+++ barchart.js
@@ -39,19 +39,28 @@ class Barchart extends Widget {
         options = (options || {});
 
         me.data = (data ? me.clean(data) : []);
+
+        // color options
         me.negColor = (options.negColor || '#dc3912');
         me.midColor = (options.midColor || 'lightgrey');
         me.posColor = (options.posColor || '#109618');
         me.numColors = (options.numColors || 256);
         me.colors = (options.colors || me.interpolateColors(me.negColor, me.midColor, me.posColor, me.numColors));
+
+        // sort/order options
         me.byName = (options.byName === undefined ? true : options.byName);
         me.ascending = (options.ascending === undefined ? true : options.ascending);
+
+        // miscellaneous options
         me.defaultDataMax = (options.defaultDataMax || 0.75);
         me.keyTooltipLabel = (options.keyTooltipLabel || 'Variable');
         me.valueTooltipLabel = (options.valueTooltipLabel || 'Coefficient');
-        me.tooltipFormat = (options.tooltipFormat || d3.format('.7'));
-        me.noTransition = options.noTransition;
 
+        // standard options
+        me.tooltipFormat = (options.tooltipFormat || d3.format('.7'));
+        me.noTransition = (options.noTransition === undefined ? false : options.noTransition);
+
+        // sort data before mapping labels
         me.sortData();
         me.labels = me.data.map(me.key);
         me.dataMax = me.getDataMax();
